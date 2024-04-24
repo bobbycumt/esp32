1b5e5dd96a12546e3f57722658478b4ee076d77d
bobbycumt <bobbycumt@163.com>
2024-04-24 14:32:12 +0800

    Update main.py

--- test/main.py
+++ test/main.py
@@ -5,7 +5,9 @@ from lib import urequests
 import ujson
 import ssd1306
 from umqtt.simple import MQTTClient
-import _thread
+import dht
+
+th=dht.DHT11(Pin(2))
 
 # i2c=I2C(sda=Pin(21),scl=Pin(22))
 # display=ssd1306.SSD1306_I2C(128,32,i2c)
@@ -24,6 +26,9 @@ TOPIC1 = "attributes/push"
 
 p13 = Pin(13, Pin.OUT)
 
+f=0
+state=0
+
 def do_connect():
     import network
     wlan = network.WLAN(network.STA_IF)
@@ -41,48 +46,55 @@ def do_connect():
         p13.value(0)
 
 def mqtt_connect():
-    client = MQTTClient(CLIENT_ID, SERVER, PORT, USERNAME, PASSWORD)
+    client = MQTTClient(CLIENT_ID, SERVER, PORT, USERNAME, PASSWORD,keepalive=600)
     client.connect()
     print('Connected to MQTT Broker "{server}"'.format(server = SERVER))
     return client
 
 def on_message(topic, msg):
     global f
+    global state
     print("Received '{payload}' from topic '{topic}'\n".format(
         payload = msg.decode(), topic = topic.decode()))
-    k=list(ujson.loads(msg.decode()).keys())
+    d=ujson.loads(msg.decode())
+    k=list(d.keys())
     if k[0]=="get":
         f=2
     elif k[0]=="state":   
         f=1
+        state=d["state"]
 def subscribe(client):
     client.set_callback(on_message)
     client.subscribe(TOPIC1)
 
 def main():
-    do_connect()
     global f
-    f=0
+    do_connect()
     client= mqtt_connect()
     subscribe(client)
     while 1:
+        th.measure()
+        t=th.temperature()
+        h=th.humidity()
         if f==1:
+            p13.value(state)
+            utime.sleep(1)
+            p13.value(0)
             msg_dict = {
-                    'tmp': 8
+                    'state': 0
                 }
             msg = ujson.dumps(msg_dict)
             result = client.publish(TOPIC, msg)
-            print("Send '{msg}' to topic '{topic}'".format(msg = msg, topic = TOPIC))
             print(1)
             f=0
         elif f==2:
             msg_dict = {
-                    'humidity': 7
+                    'tmp': t, 'humidity': h
                 }
             msg = ujson.dumps(msg_dict)
             result = client.publish(TOPIC, msg)
             print("Send '{msg}' to topic '{topic}'".format(msg = msg, topic = TOPIC))
-            print(1)
+            print(2)
             f=0
         client.wait_msg()
 
