0df52e70d3aafb2efb1daf84369f2f45e495f6bc
bobbycumt <bobbycumt@163.com>
2024-04-26 18:09:40 +0800

    update

--- doorUpDown/main.py
+++ doorUpDown/main.py
@@ -1,92 +1,149 @@
-from umqtt.simple import MQTTClient
-from machine import Pin
-import network
+# -*- coding: utf-8 -*-
+from machine import Pin,I2C
 import utime
-import random
+from lib import urequests
 import ujson
+import ssd1306
+from umqtt.simple import MQTTClient
+import dht
 
-wlan = network.WLAN(network.STA_IF)
-
+th=dht.DHT11(Pin(3))
 up = Pin(15, Pin.OUT)
 down = Pin(4, Pin.OUT)
 lock = Pin(2, Pin.OUT)
 stop = Pin(5, Pin.OUT)
+p13 = Pin(13, Pin.OUT)
 
-# ssid = 'CU_future'
-# password = '13582579999'
-ssid = 'bbhh'
-password = 'lb19850922'
+# i2c=I2C(sda=Pin(21),scl=Pin(22))
+# display=ssd1306.SSD1306_I2C(128,32,i2c)
+# display.font_load("GB2312-12.fon")
 
-client_id='pc_esp32' 
-server = 'bj-2-mqtt.iot-api.com' 
-port = 1883 #连接的端口
-user = 'gi9ruf6zithfmw64' #产品的数字ID
-pwd = 'VW6nAnxPSC'
+ssid='CU_future'
+psw='13582579999'
+# ssid = 'bbhh'
+# psw = 'lb19850922'
 
-# Received messages from subscriptions will be delivered to this callback
-c = MQTTClient(client_id, server,port,user,pwd,60) 
+SERVER = "bj-2-mqtt.iot-api.com"
+PORT = 1883
+CLIENT_ID = 'scmcfuso'
+USERNAME = 'gi9ruf6zithfmw64'
+PASSWORD = 'VW6nAnxPSC'
+TOPIC = "attributes"
+TOPIC1 = "attributes/push"
 
-def sub_cb(topic, msg):
-    # print((topic, msg))
-    msg=ujson.loads(msg)
-    # print(list(msg.keys())[0])
-    if list(msg.keys())[0]=='lock':
-        lock.value(int(msg['lock']))
-        utime.sleep(1)
-        lock.value(0)
-    if list(msg.keys())[0]=='stop':
-        stop.value(int(msg['stop']))
-        utime.sleep(1)
-        stop.value(0)
-    if list(msg.keys())[0]=='up':
-        up.value(int(msg['up']))
-        utime.sleep(1)
-        up.value(0)
-    if list(msg.keys())[0]=='down':
-        down.value(int(msg['down']))
-        utime.sleep(1)
-        down.value(0)           
+f=0
+state=0
 
 def do_connect():
-#     wlan = network.WLAN(network.STA_IF)
-    wlan.active(False)
-    utime.sleep_ms(100)
+    import network
+    wlan = network.WLAN(network.STA_IF)
     wlan.active(True)
-    # print(wlan.scan())
     if not wlan.isconnected():
         print('connecting to network...')
-        wlan.connect(ssid, password)
+        wlan.connect(ssid, psw)
         while not wlan.isconnected():
-            pass
+             p13.value(1)    
+             utime.sleep(0.1)             
+             p13.value(0)   
+             utime.sleep(0.1)
     print('network config:', wlan.ifconfig())
+    if wlan.isconnected()==True:
+        p13.value(0)
+
+def mqtt_connect():
+    client = MQTTClient(CLIENT_ID, SERVER, PORT, USERNAME, PASSWORD,keepalive=600)
+    client.connect()
+    print('Connected to MQTT Broker "{server}"'.format(server = SERVER))
+    return client
+
+def on_message(topic, msg):
+    global f
+    global state
+    print("Received '{payload}' from topic '{topic}'\n".format(
+        payload = msg.decode(), topic = topic.decode()))
+    d=ujson.loads(msg.decode())
+    k=list(d.keys())
+    if k[0]=="get":
+        f=100
+    elif k[0]=='lock':   
+        f=1
+        state=d["lock"]
+    elif k[0]=='stop':   
+        f=2
+        state=d["stop"]
+    elif k[0]=='up':   
+        f=3
+        state=d["up"]
+    elif k[0]=='down':   
+        f=4
+        state=d["down"]
+def subscribe(client):
+    client.set_callback(on_message)
+    client.subscribe(TOPIC1)
 
 def main():
-#     do_connect()
-#     c.set_callback(sub_cb)
-#     c.connect()
-#     c.subscribe("attributes/push")
-    pt=0
-    cnt=0
-    while True:
-        if not wlan.isconnected():
-            do_connect()
-            c.set_callback(sub_cb)
-            c.connect()
-            c.subscribe("attributes/push")            
-        try:
-            c.check_msg()
-        except:
-            print("error0")
-        utime.sleep_ms(100)
-        cnt+=1
-        lock.value(cnt%2)
-        if cnt>=300:
-            cnt=0
-#             c.publish(b"attributes", '{"temp": '+str(int(random.random()*100))+'}')
-            try:
-                c.ping()
-            except:
-                print("error1")
-    
-if __name__ == '__main__':
+    global f
+    do_connect()
+    client= mqtt_connect()
+    subscribe(client)
+    while 1:
+        th.measure()
+        t=th.temperature()
+        h=th.humidity()
+        if f==1:
+            lock.value(state)
+            utime.sleep(1)
+            lock.value(0)
+            msg_dict = {
+                    'lock': 0
+                }
+            msg = ujson.dumps(msg_dict)
+            result = client.publish(TOPIC, msg)
+            print(1)
+            f=0
+        elif f==2:
+            stop.value(state)
+            utime.sleep(1)
+            stop.value(0)
+            msg_dict = {
+                    'stop': 0
+                }
+            msg = ujson.dumps(msg_dict)
+            result = client.publish(TOPIC, msg)
+            print(2)
+            f=0
+        elif f==3:
+            up.value(state)
+            utime.sleep(1)
+            up.value(0)
+            msg_dict = {
+                    'up': 0
+                }
+            msg = ujson.dumps(msg_dict)
+            result = client.publish(TOPIC, msg)
+            print(3)
+            f=0
+        elif f==4:
+            down.value(state)
+            utime.sleep(1)
+            down.value(0)
+            msg_dict = {
+                    'down': 0
+                }
+            msg = ujson.dumps(msg_dict)
+            result = client.publish(TOPIC, msg)
+            print(4)
+            f=0
+        elif f==100:
+            msg_dict = {
+                    'temperature': t, 'humidity': h
+                }
+            msg = ujson.dumps(msg_dict)
+            result = client.publish(TOPIC, msg)
+            print("Send '{msg}' to topic '{topic}'".format(msg = msg, topic = TOPIC))
+            print(100)
+            f=0
+        client.wait_msg()
+
+if __name__=='__main__':
     main()
